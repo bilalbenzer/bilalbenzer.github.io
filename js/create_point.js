6c1d224266d249324ec9d052e6a9dfea63663af0
Bilal Benzer <bllbnzr@gmail.com>
2022-06-24 16:16:47 +0300

    ..

--- js/create_point.js
+++ js/create_point.js
@@ -4,7 +4,8 @@
 */
 var alfabe_harfler = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","x","w","y","z",
                       "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","R","S","T","U","X","W","Y","Y","Z",
-                    "1","2","3","4","5","6","7","8","9","0"]  
+                    "1","2","3","4","5","6","7","8","9","0"]
+var sayilar =["1","2","3","4","5","6","7","8","9","0"]  
 function create_layer_name(obje_type){ //oluşturulacak obje için kullanıcıdan katman adı istenir.
   document.getElementById("create_object").open = false
   document.getElementById("sayfamesajlari").style.backgroundColor="black";
